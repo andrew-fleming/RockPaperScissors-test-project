f5e2159e32d514aeccd6692ca5d7ce382bc10bc2
Andrew <fleming-andrew@protonmail.com>
2021-05-22 13:37:47 -0400

    Finished testing up to timeOut function, started testing outcomes

--- test/RockPaperScissors.spec.ts
+++ test/RockPaperScissors.spec.ts
@@ -1,8 +1,9 @@
 import { ethers } from "hardhat"
 import chai, { expect } from "chai";
 import { solidity } from "ethereum-waffle"
-import { Contract, BigNumber, providers } from "ethers"
+import { Contract, BigNumber } from "ethers"
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { time } from "@openzeppelin/test-helpers";
 
 chai.use(solidity)
 
@@ -17,8 +18,6 @@ describe("RockPaperScissors Contract", () => {
     let alice: SignerWithAddress
     let bob: SignerWithAddress
     let carol: SignerWithAddress
-    let dave: SignerWithAddress
-    let eve: SignerWithAddress
 
     const daiAmount: BigNumber = ethers.utils.parseEther("25000");
 
@@ -26,7 +25,7 @@ describe("RockPaperScissors Contract", () => {
         const MockDai = await ethers.getContractFactory("MockDai");
         const RockPaperScissors = await ethers.getContractFactory("RockPaperScissors");
 
-        [alice, bob, carol, dave, eve] = await ethers.getSigners();
+        [alice, bob, carol] = await ethers.getSigners();
 
         mockDai = await MockDai.deploy()
         contract = await RockPaperScissors.deploy(mockDai.address)
@@ -38,8 +37,6 @@ describe("RockPaperScissors Contract", () => {
             mockDai.mint(alice.address, daiAmount),
             mockDai.mint(bob.address, daiAmount),
             mockDai.mint(carol.address, daiAmount),
-            mockDai.mint(dave.address, daiAmount),
-            mockDai.mint(eve.address, daiAmount)
         ])
     })
 
@@ -148,13 +145,18 @@ describe("RockPaperScissors Contract", () => {
             expect(await contract.userBalance(alice.address))
                 .to.eq(ethers.utils.parseEther("24999"))
         })
+
+        it("should revert when different player calls discardCreatedGame", async() => {
+            let betAmount = ethers.utils.parseEther("5")
+            await contract.createGame(betAmount)
+
+            await expect(contract.connect(bob).discardCreatedGame(1))
+                .to.be.reverted
+        })
     })
 
     describe("joinGame function", async() => {
         it("should join open game and update variables", async() => {
-            let betAmount = ethers.utils.parseEther("5")
-            await contract.createGame(betAmount)
-
             await mockDai.connect(bob).approve(contract.address, daiAmount) 
             await contract.connect(bob).deposit(daiAmount)
             await contract.connect(bob).joinGame(1)
@@ -173,9 +175,119 @@ describe("RockPaperScissors Contract", () => {
         })
 
         it("should revert joining closed game", async() => {
-            await expect(contract.connect(bob).joinGame(1))
+            await mockDai.connect(carol).approve(contract.address, daiAmount)
+            await contract.connect(carol).deposit(daiAmount)
+            await expect(contract.connect(carol).joinGame(1))
                 .to.be.revertedWith("Either game is closed or insufficient funds")
         })
     })
 
+    describe("sendMove function", async() => {
+        it("should assign move and timestamp", async() => {
+            // Params => gameId && move (1 for ROCK)
+            await contract.sendMove(1, 1)
+
+            res = await contract.games(1)
+            expect(res[2]).to.eq(1)
+
+            res = await contract.games(1)
+            expect(res[4]).to.be.greaterThan(0)
+        })
+
+        it("should revert function call from non-player", async() => {
+            await expect(contract.connect(carol).sendMove(1, 3))
+                .to.be.revertedWith("Either incorrect gameId or move already sent")
+        })
+
+        it("should revert from player resending a move", async() => {
+            await expect(contract.sendMove(1, 3))
+                .to.be.revertedWith("Either incorrect gameId or move already sent")
+        })
+
+        it("should revert from timeout", async() => {
+            await time.increase(300)
+            await expect(contract.connect(bob).sendMove(1, 3))
+                .to.be.revertedWith("Timed out")
+        })
+    })
+
+    describe("Timeout function", async() => {
+        it("should revert when non-player calls function", async() => {
+            await expect(contract.connect(carol).timeOut(1))
+                .to.be.revertedWith("Either game did not time out or player not in game")
+        })
+
+        it("should update balances with delayFee", async() => {
+            let aliceBalance = await contract.userBalance(alice.address)
+            let bobBalance = await contract.userBalance(bob.address)
+
+            // Baseline check
+            expect(aliceBalance.toString())
+                .to.eq(ethers.utils.parseEther("24994"))
+
+            expect(bobBalance.toString())
+                .to.eq(ethers.utils.parseEther("24995"))
+
+            await contract.timeOut(1)
+
+            aliceBalance = await contract.userBalance(alice.address)
+            bobBalance = await contract.userBalance(bob.address)
+
+            // Updated balance check
+            expect(aliceBalance.toString())
+                .to.eq(ethers.utils.parseEther("25000"))
+
+            expect(bobBalance.toString())
+                .to.eq(ethers.utils.parseEther("24999"))
+        })
+    })
+})
+
+describe("Starting from deployment", () => {
+
+    let res: any
+
+    let contract: Contract
+    let mockDai: Contract
+
+    let alice: SignerWithAddress
+    let bob: SignerWithAddress
+
+    const daiAmount: BigNumber = ethers.utils.parseEther("25000");
+
+    beforeEach(async() => {
+        const MockDai = await ethers.getContractFactory("MockDai");
+        const RockPaperScissors = await ethers.getContractFactory("RockPaperScissors");
+
+        [alice, bob] = await ethers.getSigners();
+
+        mockDai = await MockDai.deploy()
+        contract = await RockPaperScissors.deploy(mockDai.address)
+
+        /**
+         * Mint, approve, and deposit mockDai for alice and bob
+         */
+        await Promise.all([
+            mockDai.mint(alice.address, daiAmount),
+            mockDai.mint(bob.address, daiAmount),
+            mockDai.approve(contract.address, daiAmount),
+            mockDai.connect(bob).approve(contract.address, daiAmount),
+            contract.deposit(daiAmount),
+            contract.connect(bob).deposit(daiAmount)
+        ])
+
+        let betAmount = ethers.utils.parseEther("10")
+        await contract.createGame(betAmount)
+        await contract.connect(bob).joinGame(0)
+    })
+
+    describe("calculates correct outcomes for finishGame function", async() => {
+        it("rock vs rock", async() => {
+            await contract.sendMove(0, 1)
+        })
+
+        it("calculates gg", async() => {
+            await contract.sendMove(0, 1)
+        })
+    })
 })
