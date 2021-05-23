eb457ec110168da4167492c3b9ab19ad727ee632
Andrew <fleming-andrew@protonmail.com>
2021-05-23 04:30:34 -0400

    Finished comments

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -1,12 +1,11 @@
 pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 /// @title Rock, paper, scissors game played with ERC20 tokens
 /// @author Andrew Fleming
-/// @notice Use this contract to create a rock, paper, scissors factory
-/// @dev Player moves are not encoded
+/// @notice Use this contract to play rock, paper, scissors while betting with DAI stablecoin
+/// @dev Game factory. Player moves are not encoded
 contract RockPaperScissors {
 
     struct Game {
@@ -14,7 +13,7 @@ contract RockPaperScissors {
         address playerTwo;
         uint8 p1Move;
         uint8 p2Move;
-        uint32 startTime;
+        uint256 startTime;
         uint256 _gameId;
         uint256 betAmount;
     }
@@ -23,17 +22,23 @@ contract RockPaperScissors {
     mapping(address => uint256) public userBalance;
     // gameId => openGame
     mapping(uint256 => bool) public openGame;
+    // gameId => gameFinished
+    mapping(uint256 => bool) public gameFinished;
     // p1Move => p2Move => isP1Winner
     mapping(uint8 => mapping(uint8 => bool)) public isP1Winner;
     
     uint256 public gameId; // Game identifier
 
-    IERC20 public daiToken; // DAI token instance
+    IERC20 public daiToken; // DAI state variable
 
     Game[] public games; // Array to store game data
+
+    event Outcome(uint256 indexed gameId, address indexed winner); // Sole event created for testing
     
     /// @notice Connects to ERC20 interface
-    /// @dev isP1Winner mappings set as the only winning combinations in game for player one
+    /// @dev isP1Winner mapping sets the only winning combinations in the game for player one.
+    ///      Other combinations equate to false; ergo, player two wins. The finishGame function
+    ///      evaluates for draws prior to utilizing isP1Winner
     /// @param _daiToken Requisite address for DAI stablecoin
     constructor(IERC20 _daiToken) {
         daiToken = _daiToken;
@@ -42,10 +47,10 @@ contract RockPaperScissors {
         isP1Winner[3][2] = true;
     }
     
-    /// @notice Creates a game instance whereby sender declares the bet amount (including zero)
-    /// @dev After creating the struct object, the openGame mapping is set to true and the global 
-    ///      gameId increases by one
-    /// @param _betAmount The cost to play as chosen by the game maker
+    /// @notice Creates a game instance whereby the user declares the bet amount (including zero)
+    /// @dev After creating the struct object, the openGame mapping is set to true and the state 
+    ///      variable gameId increases by one
+    /// @param _betAmount The cost to play(can be zero) as chosen by the caller
     function createGame(uint _betAmount) public {
         require(userBalance[msg.sender] >= _betAmount, 
             "Insufficient funds"
@@ -60,9 +65,14 @@ contract RockPaperScissors {
         gameId++;
     }
 
-    /// @notice 
-    /// @dev 
-    /// @param _gameId ghghg
+    /// @notice The next step after createGame in the game's lifecycle. This function is for users
+    ///         who want to play without creating their own game or want to join a particular user's
+    ///         game (as long as they know their gameId)
+    /// @dev Assigns the joining player's address into the Game struct as well as removing the
+    ///      bet amount from their balance. This incentivizes users to participate in the games
+    ///      they join. The openGame mapping is assigned to false. This prevents other users from
+    ///      overriding the initial joiner as checked by the require statement
+    /// @param _gameId An unsigned integer used to identify specific a game
     function joinGame(uint _gameId) public {
         require(
             openGame[_gameId] == true &&
@@ -75,55 +85,71 @@ contract RockPaperScissors {
         userBalance[msg.sender] -= game.betAmount;
     }
 
-    /// @notice 
-    /// @dev 
-    /// @param _gameId ghghgh
-    /// @param _choice ghghg
-    function sendMove(uint256 _gameId, uint8 _choice) public {
+    /// @notice Sends the user's choice of rock, paper, or scissors to the contract. If the
+    ///         user is first to send his/her move, the five minute timer begins. Otherwise,
+    ///         the contract compares both players' moves and updates their DAI balances
+    ///         accordingly
+    /// @dev The first conditional statement assigns the user's move to the Game struct. Thereafter,
+    ///      it evaluates if the startTime timer was declared. If so, the contract calls the 
+    ///      private finishGame function. If the timer equates to zero, however, the contract
+    ///      assigns it to the current timestamp
+    /// @param _gameId An unsigned integer used to identify specific a game
+    /// @param _move The user's choice of rock, paper, or scissors
+    function sendMove(uint256 _gameId, uint8 _move) public {
         Game storage game = games[_gameId];
         require(
+            gameFinished[_gameId] == false &&
             msg.sender == game.playerOne && game.p1Move == 0 ||
             msg.sender == game.playerTwo && game.p2Move == 0,
-            "Either incorrect gameId or move already sent"
+            "Either incorrect gameId, move already sent, or game finished"
         );
         if(msg.sender == game.playerOne){
-            game.p1Move = _choice;
+            game.p1Move = _move;
         } else {
-            game.p2Move = _choice;
+            game.p2Move = _move;
         }
 
         if(game.startTime == 0){
-            game.startTime = SafeCast.toUint32(block.timestamp);
-\        } else {
+            game.startTime = block.timestamp;
+        } else {
             require(game.startTime + 300 >= block.timestamp, "Timed out");
             finishGame(_gameId);
+            gameFinished[_gameId] = true;
         }
     }
 
     /// @notice Determines the outcome and updates participating users' balances
     /// @dev The first conditional statement checks if the game is a tie. If not, the contract inserts
     ///      the players' moves into the isP1Winner mapping. If it matches one of the combinations hardcoded
-    ///      in the constructor, player one wins
+    ///      in the constructor, player one's userBalance increases by 2x the bet. Otherwise,
+    ///      player two's balance is updated
     /// @param _gameId An unsigned integer used to identify specific a game
     function finishGame(uint _gameId) private {
         Game memory game = games[_gameId];
         if(game.p1Move == game.p2Move){
                 userBalance[game.playerOne] += game.betAmount;
                 userBalance[game.playerTwo] += game.betAmount;
+                emit Outcome(_gameId, address(0));
         } else {
-            bool res = isP1Winner[game.p1Move][game.p2Move];
+            bool res = isP1Winner[game.p1Move][game.p2Move]; 
             uint256 winAmount = game.betAmount * 2;
 
             if(res == true) {
                 userBalance[game.playerOne] += winAmount;
+                emit Outcome(_gameId, game.playerOne);
             } else {
                 userBalance[game.playerTwo] += winAmount;
+                emit Outcome(_gameId, game.playerTwo);
             }
         }
     }
 
-    /// @notice 
-    /// @dev 
+    /// @notice Should be used when a player does not want to keep his/her game open while
+    ///         waiting for an opponent
+    /// @dev Updates the openGame mapping to false which blocks users from joining this particular game.
+    ///      Further, the user receives their bet amount back in the balance. Finally, the gameFinished
+    ///      mapping updates to true; as, this prevents users from calling the sendMove function
+    ///      to the discarded game's gameId
     /// @param _gameId An unsigned integer used to identify specific a game
     function discardCreatedGame(uint256 _gameId) public {
         require(
@@ -133,19 +159,24 @@ contract RockPaperScissors {
         );
         openGame[_gameId] = false;
         userBalance[msg.sender] += games[_gameId].betAmount;
+        gameFinished[_gameId] = true;
     }
 
-    /// @notice
-    /// @dev
+    /// @notice Serves as a deterrent for uncooperative players. The active player can call
+    ///         this function five minutes after sending their move. The uncooperative player
+    ///         receives 80% of his/her bet and the active player receives 120%
+    /// @dev Despite the potential for a 15 second discrepancy in the timestamp, 300 seconds
+    ///      (5 minutes) seems reasonable for this instance
     /// @param _gameId An unsigned integer used to identify specific a game
     function timeOut(uint _gameId) public {
         Game memory game = games[_gameId];
         require(
-            game.startTime + 300 < block.timestamp && // Greater than 5 minutes
+            gameFinished[_gameId] == false &&
             msg.sender == game.playerOne || 
             msg.sender == game.playerTwo,
-            "Either game did not time out or player not in game"
+            "Either player not in game or game is finished"
         );
+        require(game.startTime + 300 < block.timestamp, "Time not exceeded");
         uint256 delayFee = game.betAmount / 5;
         if(game.p1Move == 0){
             userBalance[game.playerOne] += delayFee * 4;
@@ -154,6 +185,7 @@ contract RockPaperScissors {
             userBalance[game.playerTwo] += delayFee * 4;
             userBalance[game.playerOne] += game.betAmount + delayFee;
         }
+        gameFinished[_gameId] = true;
     }
 
     /// @notice Sends the requested DAI amount to the contract which serves as an enrollment to play
@@ -169,12 +201,11 @@ contract RockPaperScissors {
     }
 
     /// @notice Sends the requested DAI amount back to the user
-    /// @dev 
+    /// @dev Updates userBalance mapping and sends a transaction with the amount to the user
     /// @param amount The quantity of DAI for the user to receive
     function withdraw(uint amount) public {
         require(userBalance[msg.sender] >= amount, "Insufficient funds");
         userBalance[msg.sender] -= amount;
         daiToken.transfer(msg.sender, amount);
     }
-
 }
--- contracts/mocks/MockDai.sol
+++ contracts/mocks/MockDai.sol
@@ -1,13 +1,12 @@
 pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MockDai is ERC20, Ownable {
+contract MockDai is ERC20 {
 
     constructor() ERC20("MockDai", "mDai") {}
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
 }
